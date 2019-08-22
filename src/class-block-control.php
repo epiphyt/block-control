5544b43041fb3e2926193357de67c3d1ae02f3b0
Matthias Kittsteiner <mk@kittmedia.com>
2019-08-22 09:50:50 +0200

    :sparkles: Add mobile detection and properly hide blocks in the frontend

--- src/class-block-control.php
+++ src/class-block-control.php
@@ -1,6 +1,8 @@
 <?php
 namespace epiphyt\Block_Control;
+use Mobile_Detect;
 use function add_action;
+use function add_filter;
 use function dirname;
 use function file_exists;
 use function load_plugin_textdomain;
@@ -16,9 +18,14 @@ use function wp_enqueue_script;
  */
 class Block_Control {
 	/**
-	 * @var		\epiphyt\Block_Control\Block_Control;
+	 * @var		\epiphyt\Block_Control\Block_Control
 	 */
 	public static $instance;
+	
+	/**
+	 * @var		\Mobile_Detect The Mobile Detect instance
+	 */
+	public $mobile_detect;
 	
 	/**
 	 * @var		string The plugin filename
@@ -29,7 +36,10 @@ class Block_Control {
 	 * Block_Control constructor.
 	 */
 	public function __construct() {
+		require_once 'lib/class-mobile-detect.php';
+		
 		self::$instance = $this;
+		$this->mobile_detect = new Mobile_Detect();
 	}
 	
 	/**
@@ -38,6 +48,7 @@ class Block_Control {
 	public function init() {
 		add_action( 'enqueue_block_editor_assets', [ $this, 'editor_assets' ] );
 		add_action( 'init', [ $this, 'load_textdomain' ], 0 );
+		add_filter( 'render_block', [ $this, 'toggle_blocks' ], 10, 2 );
 	}
 	
 	/**
@@ -61,6 +72,51 @@ class Block_Control {
 	}
 	
 	/**
+	 * Test if the content should be hidden by its attributes.
+	 * 
+	 * @param	string		$attr The attribute name
+	 * @param	bool		$value The attribute value
+	 * @return	bool True if the content should be hidden, false otherwise
+	 */
+	private function hide_desktop( $attr, $value ) {
+		if ( $attr === 'hide_desktop' && $value === true && ! $this->mobile_detect->isMobile() && ! $this->mobile_detect->isTablet() ) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Test if the content should be hidden by its attributes.
+	 * 
+	 * @param	string		$attr The attribute name
+	 * @param	bool		$value The attribute value
+	 * @return	bool True if the content should be hidden, false otherwise
+	 */
+	private function hide_mobile( $attr, $value ) {
+		if ( $attr === 'hide_mobile' && $value === true && $this->mobile_detect->isMobile() ) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Test if the content should be hidden by its attributes.
+	 * 
+	 * @param	string		$attr The attribute name
+	 * @param	bool		$value The attribute value
+	 * @return	bool True if the content should be hidden, false otherwise
+	 */
+	private function hide_tablet( $attr, $value ) {
+		if ( $attr === 'hide_tablet' && $value === true && $this->mobile_detect->isTablet() ) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
 	 * Load translations.
 	 */
 	public function load_textdomain() {
@@ -76,5 +132,37 @@ class Block_Control {
 		if ( file_exists( $file ) ) {
 			$this->plugin_file = $file;
 		}
+	}
+	
+	/**
+	 * Display or hide a block.
+	 * 
+	 * @param	string		$block_content The block content about to be appended
+	 * @param	array		$block The full block, including name and attributes
+	 * @return	string The updated block content
+	 */
+	public function toggle_blocks( $block_content, $block ) {
+		// set default content
+		$content = '';
+		
+		// iterate through all block attributes
+		foreach ( $block['attrs'] as $attr => $value ) {
+			if ( $this->hide_desktop( $attr, $value ) ) {
+				break;
+			}
+			
+			if ( $this->hide_mobile( $attr, $value ) ) {
+				break;
+			}
+			
+			if ( $this->hide_tablet( $attr, $value ) ) {
+				break;
+			}
+			
+			// get the block content to output it
+			$content = $block_content;
+		}
+		
+		return $content;
 	}
 }
