332296d3f402a18ccdcee6ad73c91fbb53449c48
Matthias Kittsteiner <mk@kittmedia.com>
2019-11-06 18:27:40 +0100

    :bulb: Update version in the files to v1.0.2 See #9

--- inc/class-block-control.php
+++ inc/class-block-control.php
@@ -20,7 +20,7 @@ use function wp_set_script_translations;
  * 
  * @author	Epiphyt
  * @license	GPL2 <https://www.gnu.org/licenses/gpl-2.0.html>
- * @version	1.0.1
+ * @version	1.0.2
  */
 class Block_Control {
 	/**
